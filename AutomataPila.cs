735cc2e8ba24875ec2eb0c61fe4732cef6bd4ff4
Carlos Barrientos <93161779+barryways@users.noreply.github.com>
2024-08-08 09:49:44 -0600

    Add project

---
+++ AutomataPila.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiladores_P1_1040121
+{
+    public class AutomataPila
+    {
+        private Stack<string> pila;
+        public AutomataPila()
+        {
+            pila = new Stack<string>();
+        }
+        public int parsear(List<string> tokens)
+        {
+            pila.Push("S");
+
+            while (tokens.Count() != 0)
+            {
+                string lookahead = tokens[0];
+                switch (pila.Peek())
+                {
+                    case "if":
+                        {
+                            if (pila.Peek() == lookahead)
+                            {
+                                tokens.Remove(tokens[0]);
+                                pila.Pop();
+                            }
+                            else
+                            {
+                                throw new Exception("Se esperaba " + pila.Peek() + " y se encontro " + lookahead);
+                            }
+                        }
+                        break;
+                    case "else_if":
+                        {
+                            if (pila.Peek() == lookahead)
+                            {
+                                tokens.Remove(tokens[0]);
+                                pila.Pop();
+                            }
+                            else
+                            {
+                                throw new Exception("Se esperaba " + pila.Peek() + " y se encontro " + lookahead);
+                            }
+                        }
+                        break;
+                    case "else":
+                        {
+                            if (pila.Peek() == lookahead)
+                            {
+                                tokens.Remove(tokens[0]);
+                                pila.Pop();
+                            }
+                            else
+                            {
+                                throw new Exception("Se esperaba " + pila.Peek() + " y se encontro " + lookahead);
+                            }
+                        }
+                        break;
+                    case "then":
+                        {
+                            if (pila.Peek() == lookahead)
+                            {
+                                tokens.Remove(tokens[0]);
+                                pila.Pop();
+                            }
+                            else
+                            {
+                                throw new Exception("Se esperaba " + pila.Peek() + " y se encontro " + lookahead);
+                            }
+                        }
+                        break;
+                    case "ID":
+                        {
+                            if (pila.Peek() == lookahead)
+                            {
+                                tokens.Remove(tokens[0]);
+                                pila.Pop();
+                            }
+                            else
+                            {
+                                throw new Exception("Se esperaba " + pila.Peek() + " y se encontro " + lookahead);
+                            }
+                        }
+                        break;
+                    case "Num":
+                        {
+                            if (pila.Peek() == lookahead)
+                            {
+                                tokens.Remove(tokens[0]);
+                                pila.Pop();
+                            }
+                            else
+                            {
+                                throw new Exception("Se esperaba " + pila.Peek() + " y se encontro " + lookahead);
+                            }
+                        }
+                        break;
+                    case "eof":
+                        {
+                            if (pila.Peek() == lookahead)
+                            {
+                                tokens.Remove(tokens[0]);
+                                pila.Pop();
+                            }
+                            else
+                            {
+                                throw new Exception("Se esperaba " + pila.Peek() + " y se encontro " + lookahead);
+                            }
+                        }
+                        break;
+
+                    case "if_stmt":
+                        {
+                            pila.Pop();
+                            pila.Push("eof");
+                            pila.Push("else_part");
+                            pila.Push("else_if_part");
+                            pila.Push("stmt");
+                            pila.Push("then");
+                            pila.Push("expr");
+                            pila.Push("if");
+                        }
+                        break;
+                    case "else_if_part":
+                        {
+                            if (lookahead == "else_if")
+                            {
+                                pila.Pop();
+                                pila.Push("expr");
+                                pila.Push("then");
+                                pila.Push("stmt");
+                                pila.Push("else_if_part");
+                                pila.Push("else_if");
+                            }
+                            else
+                            {
+                                pila.Pop();
+                            }
+                        }
+                        break;
+                    case "else_part":
+                        {
+                            if (lookahead == "else")
+                            {
+
+                                pila.Pop();
+                                pila.Push("stmt");
+                                pila.Push("else");
+                            }
+                            else
+                            {
+                                pila.Pop();
+                            }
+                        }
+                        break;
+                    case "stmt":
+                        {
+                            if (lookahead == "ID")
+                            {
+                                pila.Pop();
+                                pila.Push("ID");
+                            }
+                            else
+                            {
+                                pila.Pop();
+                                pila.Push("Num");
+                            }
+
+                        }
+                        break;
+                    case "expr":
+                        {
+                            if (lookahead == "ID")
+                            {
+                                pila.Pop();
+                                pila.Push("ID");
+                            }
+                            else
+                            {
+                                pila.Pop();
+                                pila.Push("Num");
+                            }
+                        }
+                        break;
+
+                    case "S":
+                        {
+                            pila.Pop();
+                            pila.Push("if_stmt");
+                        }
+                        break;
+
+                }
+            }
+            if (pila.Count() > 0)
+                return 0;
+            else
+                return 1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Compiladores_P1_1040121
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                string cadenaEntrada = "";
+                do
+                {
+                    Console.WriteLine("Esciba la expresion a utilizar\nUn ejemplo puede ser \"if variable1 then 8\"\nEstas son las palabras reservadas \"if\", \"then\", \"else_if\", \"else\"");
+                    cadenaEntrada = Console.ReadLine();
+                } while (cadenaEntrada =="");
+
+
+                AutomataPila automataPila = new AutomataPila();
+                List<string> tokens = tokenizar(cadenaEntrada);
+
+                int resultado = automataPila.parsear(tokens);
+
+                if (resultado == 0)
+                {
+                    Console.WriteLine("El programa detecto que la Pila no esta vacia, por lo cual quedo mal la pila");
+                }
+                else
+                {
+                    Console.WriteLine("Todo salio bien, el automata pudo parsear la expresion");
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+            Console.ReadKey();
+        }
+
+        static private List<string> tokenizar(string cadenaEntada)
+        {
+            List<string> tokens = new List<string>();
+            string[] palabras = cadenaEntada.Split(' ');
+            foreach (string palabra in palabras)
+            {
+                if (palabra == "if" || palabra == "then" || palabra == "else_if" || palabra =="else")
+                {
+                    tokens.Add(palabra);
+                }
+                else
+                {
+                    if (Regex.IsMatch(palabra, "^[a-zA-Z_][a-zA-Z0-9_]*$"))
+                    {
+                        tokens.Add("ID");
+                    }
+                    else if (Regex.IsMatch(palabra, "^[0-9]*$"))
+                    {
+                        tokens.Add("Num");
+                    }
+                }
+
+            }
+            tokens.Add("eof");
+            return tokens;
+
+        }
+    }
+}
